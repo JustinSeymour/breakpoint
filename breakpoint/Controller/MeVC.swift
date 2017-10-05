c7cc01d20b82822f553e51c16b415b88405788bb
Justin Seymour <justin@3wmholdings.com>
2017-10-05 10:39:44 +0200

    Lesson144 + auth service inclusion

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,29 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Justin Seymour on 2017/10/05.
+//  Copyright © 2017 Justin Seymour. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func SignoutBtnWasPressed(_ sender: Any) {
+        // Sign Out Code
+    }
+    
+    
+
+}
